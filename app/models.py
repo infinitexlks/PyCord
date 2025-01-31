64acd890e242a1f4253999551ca0d0c1fa41f6a4
Infinite <jotarokujo0539@gmail.com>
2025-01-31 12:21:51 -0500

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from app.extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    messages = db.relationship('Message', backref='author', lazy=True)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
